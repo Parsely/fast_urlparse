b341418a36326be37a2c11b5f14b0681665fdaf6
Dan Blanchard <dan.blanchard@gmail.com>
2016-10-25 13:25:56 -0400

    Add instructions to setup.py

--- setup.py
+++ setup.py
@@ -3,3 +3,6 @@ from Cython.Build import cythonize
 
 setup(name="urlparse",
       ext_modules=cythonize('urlparse.py'))  # Change to .pyx when we have one
+
+# To build this...
+# python setup.py build_ext --inplace
